d86a974ba15ff7a799f25f77df5572fe0915f23b
Cristina Hettino <cristynohecedeno@gmail.com>
2026-07-08 22:08:04 -0500

    demo: prueba de fuego milla 2

--- internal/handlers/handler_transporte/solicitud_handler_test.go
+++ internal/handlers/handler_transporte/solicitud_handler_test.go
@@ -35,7 +35,7 @@ func TestCrearSolicitud_Exitoso(t *testing.T) {
 
 	h.ServeHTTP(rec, req)
 
-	require.Equal(t, http.StatusCreated, rec.Code)
+	require.Equal(t, http.StatusTeapot, rec.Code)
 
 	var creada modelos.Solicitud
 
